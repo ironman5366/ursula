27c347908b8b28dd04268a142955eb5fe02e42ee
Will Beddow <will@willbeddow.com>
2023-07-22 14:08:33 -0400

    New migration

---
+++ supabase/migrations/20230722173757_add_reivew_uid_constraint.sql
@@ -0,0 +1,14 @@
+CREATE POLICY "Users can only create a review for themself"
+    ON public.reviews FOR INSERT WITH CHECK(
+        auth.jwt() ->> 'uid' = 'user_uid'
+    );
+
+CREATE POLICY "Users can only update a review for themself"
+    ON public.reviews FOR UPDATE USING (
+        auth.jwt() ->> 'uid' = 'user_uid'
+    );
+
+CREATE POLICY "Anyone can view reviews"
+    ON public.reviews FOR SELECT USING (
+        true
+    );
