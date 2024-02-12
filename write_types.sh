643d793b1e851e8c0506bc440c5b860ce7b26e58
Will Beddow <will@willbeddow.com>
2024-02-12 14:17:43 -0500

    monorepo setup

--- write_types.sh
+++ write_types.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-npx supabase gen types typescript --linked > .database.temp.ts
-cp .database.temp.ts mobile/types/Database.ts
-cp .database.temp.ts supabase/functions/book-search/types/Database.ts
-rm .database.temp.ts
+npx 
 deno fmt
