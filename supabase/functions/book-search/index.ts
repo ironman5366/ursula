7506c0fc17bba74c581af46c358829f07db400bc
Will Beddow <will@willbeddow.com>
2024-02-12 16:40:51 -0500

    Make grouping algorithm slightly more sophisticated

--- supabase/functions/book-search/index.ts
+++ supabase/functions/book-search/index.ts
@@ -11,23 +11,41 @@ type BooksByAuthorKey = {
 
 /**
  * A heuristic-based approach for grouping a list of volumes into editions and deciding which editions are part of the
- * same book. If the (sorted) author list is an exact string match, we calculate the Levenshtein distance between the
- * titles. If the Levenshtein distance divided by the length of the length of the string is < 25%
- * (read: "if the strings are less than 25% different), we consider the volumes to be part of the same book.
+ * same book. If the (sorted) author list is an exact string match, we calculate the Levenshtein distance between a slice of the titles.
+ * We slice the titles by taking both to be the length of the shorter of the two.
+ * If the Levenshtein distance divided by the length of the length of the string is < 25%
+ * (read: "if the strings are less than 35% different), we consider the volumes to be part of the same book.
  * We may eventually want to replace this with a more sophisticated approach, or just farm it out to ChatGPT
  */
 function groupVolumesToBooks(volumes: VolumeSearchResponse): BooksByAuthorKey {
   const booksByAuthorKey: BooksByAuthorKey = {};
 
   for (const volume of volumes.items) {
-    const authorKey = volume.volumeInfo.authors?.sort().join(", ") ?? "";
+    const authorKey =
+      volume.volumeInfo.authors
+        ?.sort()
+        .map((author) => author.toLowerCase())
+        .join(", ") ?? "";
+
     const existingAuthorBooks = booksByAuthorKey[authorKey];
 
     if (existingAuthorBooks) {
       let satisfied = false;
       for (const title of Object.keys(existingAuthorBooks)) {
-        const distance = levenshteinDistance(title, volume.volumeInfo.title);
-        if (distance / title.length <= 0.25) {
+        // Find the shorter title
+        const shortestLength = Math.min(
+          title.length,
+          volume.volumeInfo.title.length
+        );
+
+        const titleSlice = title.slice(0, shortestLength);
+        const volumeTitleSlice = volume.volumeInfo.title.slice(
+          0,
+          shortestLength
+        );
+
+        const distance = levenshteinDistance(titleSlice, volumeTitleSlice);
+        if (distance / shortestLength <= 0.35) {
           existingAuthorBooks[title].push(volume);
           satisfied = true;
           break;
@@ -51,7 +69,6 @@ function groupVolumesToBooks(volumes: VolumeSearchResponse): BooksByAuthorKey {
 async function getOrCreateBook(name: string) {
   const volumes = await searchVolumes(name);
   const grouped = groupVolumesToBooks(volumes);
-  console.log("grouped", grouped);
 }
 
 async function handler(req: Request): Promise<Response> {
