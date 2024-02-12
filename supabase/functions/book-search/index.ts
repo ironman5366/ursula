dea26222fdc651c98c3b33df6e6f1a1bbd9fdf0c
Will Beddow <will@willbeddow.com>
2024-02-12 16:36:20 -0500

    first take on book-grouping heuristic

--- supabase/functions/book-search/index.ts
+++ supabase/functions/book-search/index.ts
@@ -1,9 +1,57 @@
-import { Book } from "@ursula/shared-types/derived.ts";
 import { searchVolumes } from "./googleBooks.ts";
+import VolumeSearchResponse from "./types/VolumeSearchResponse.ts";
+import { levenshteinDistance } from "./utils.ts";
+import Volume from "./types/Volume.ts";
+
+type BooksByAuthorKey = {
+  [authorKey: string]: {
+    [title: string]: Volume[];
+  };
+};
+
+/**
+ * A heuristic-based approach for grouping a list of volumes into editions and deciding which editions are part of the
+ * same book. If the (sorted) author list is an exact string match, we calculate the Levenshtein distance between the
+ * titles. If the Levenshtein distance divided by the length of the length of the string is < 25%
+ * (read: "if the strings are less than 25% different), we consider the volumes to be part of the same book.
+ * We may eventually want to replace this with a more sophisticated approach, or just farm it out to ChatGPT
+ */
+function groupVolumesToBooks(volumes: VolumeSearchResponse): BooksByAuthorKey {
+  const booksByAuthorKey: BooksByAuthorKey = {};
+
+  for (const volume of volumes.items) {
+    const authorKey = volume.volumeInfo.authors?.sort().join(", ") ?? "";
+    const existingAuthorBooks = booksByAuthorKey[authorKey];
+
+    if (existingAuthorBooks) {
+      let satisfied = false;
+      for (const title of Object.keys(existingAuthorBooks)) {
+        const distance = levenshteinDistance(title, volume.volumeInfo.title);
+        if (distance / title.length <= 0.25) {
+          existingAuthorBooks[title].push(volume);
+          satisfied = true;
+          break;
+        }
+      }
+
+      // If we got to the end and didn't find a suitable match, we'll consider this a new book
+      if (!satisfied) {
+        booksByAuthorKey[authorKey][volume.volumeInfo.title] = [volume];
+      }
+    } else {
+      booksByAuthorKey[authorKey] = {
+        [volume.volumeInfo.title]: [volume],
+      };
+    }
+  }
+
+  return booksByAuthorKey;
+}
 
 async function getOrCreateBook(name: string) {
   const volumes = await searchVolumes(name);
-  console.log("volumes are ", volumes);
+  const grouped = groupVolumesToBooks(volumes);
+  console.log("grouped", grouped);
 }
 
 async function handler(req: Request): Promise<Response> {
@@ -16,6 +64,8 @@ async function handler(req: Request): Promise<Response> {
       status: 400,
     });
   }
+
+  await getOrCreateBook(name);
 
   return new Response(JSON.stringify({ name }));
 }
--- supabase/functions/book-search/utils.ts
+++ supabase/functions/book-search/utils.ts
@@ -1,8 +1,9 @@
 import { PostgrestResponse } from "https://esm.sh/v131/@supabase/postgrest-js@1.8.0/dist/module/types.d.ts";
 
-export function coerceSingleResponse<T extends { id: number }>(
-  { data, error }: PostgrestResponse<T>,
-): T | null {
+export function coerceSingleResponse<T extends { id: number }>({
+  data,
+  error,
+}: PostgrestResponse<T>): T | null {
   if (error) {
     return null;
   }
@@ -19,7 +20,7 @@ export function coerceSingleResponse<T extends { id: number }>(
 }
 
 export function coerceId<T extends { id: number }>(
-  resp: PostgrestResponse<T>,
+  resp: PostgrestResponse<T>
 ): number | null {
   const singleResponse = coerceSingleResponse(resp);
   if (singleResponse === null) {
@@ -27,4 +28,25 @@ export function coerceId<T extends { id: number }>(
   }
 
   return singleResponse.id;
+}
+
+// Copied from https://www.30secondsofcode.org/js/s/levenshtein-distance/
+export function levenshteinDistance(source: string, target: string) {
+  if (!source.length) return target.length;
+  if (!target.length) return source.length;
+  const arr = [];
+  for (let i = 0; i <= target.length; i++) {
+    arr[i] = [i];
+    for (let j = 1; j <= source.length; j++) {
+      arr[i][j] =
+        i === 0
+          ? j
+          : Math.min(
+              arr[i - 1][j] + 1,
+              arr[i][j - 1] + 1,
+              arr[i - 1][j - 1] + (source[j - 1] === target[i - 1] ? 0 : 1)
+            );
+    }
+  }
+  return arr[target.length][source.length];
 }
