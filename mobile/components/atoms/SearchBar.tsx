66e9fd8f79d9f30cb9f2de31dc6fc61f1dfc55f0
Will Beddow <will@willbeddow.com>
2024-03-02 14:10:12 -0800

    ugly little "clear" button

--- mobile/components/atoms/SearchBar.tsx
+++ mobile/components/atoms/SearchBar.tsx
@@ -2,31 +2,69 @@ import React from "react";
 import { TextInput, View } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useThemeColor } from "../../theme.ts";
+import StyledButton from "../organisms/StyledButton.tsx";
 
-export default function SearchBar(
-  props: Omit<React.ComponentProps<typeof TextInput>, "style" | "placeholder">
-) {
+interface SearchBarProps
+  extends Omit<
+    React.ComponentProps<typeof TextInput>,
+    "style" | "placeholder" | "onChange" | "onChangeText" | "value"
+  > {
+  value: string;
+  // Must provide onChangeText, can't use onChange
+  onChangeText: (text: string) => void;
+}
+
+export default function SearchBar({
+  onChangeText,
+  value,
+  ...props
+}: SearchBarProps) {
   const tint = useThemeColor("tint");
   return (
     <View
       style={{
-        borderColor: tint,
-        borderWidth: 3,
-        borderRadius: 8,
         width: "100%",
-        padding: 8,
         flexDirection: "row",
       }}
     >
-      <Ionicons name={"ios-search"} size={20} color={tint} />
-      <TextInput
+      <View
         style={{
-          textAlign: "center",
+          flexDirection: "column",
+          width: "100%",
         }}
-        caretHidden={false}
-        placeholder={"Search"}
-        {...props}
-      />
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            borderColor: tint,
+            borderWidth: 3,
+            borderRadius: 8,
+            padding: 8,
+          }}
+        >
+          <Ionicons name={"ios-search"} size={20} color={tint} />
+          <TextInput
+            style={{
+              textAlign: "center",
+            }}
+            caretHidden={false}
+            placeholder={"Search"}
+            onChangeText={onChangeText}
+            value={value}
+            {...props}
+          />
+        </View>
+        {value && value.length > 0 && (
+          <View
+            style={{
+              paddingTop: 10,
+              alignSelf: "center",
+            }}
+          >
+            <StyledButton title={"Clear"} onPress={() => onChangeText("")} />
+          </View>
+        )}
+      </View>
     </View>
   );
 }
