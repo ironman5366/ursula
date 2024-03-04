2b09396ff0f62ea066becf9decdaf819b68b3894
Will Beddow <will@willbeddow.com>
2024-03-04 16:01:51 -0500

    Update EAS config

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -1,48 +1,39 @@
 module.exports = {
-  name: "ursula",
-  slug: "ursula",
-  version: "1.0.1",
-  orientation: "portrait",
-  icon: "./assets/images/logo-192.png",
-  scheme: "ursula",
-  userInterfaceStyle: "automatic",
-  splash: {
-    image: "./assets/images/logo.png",
-    resizeMode: "contain",
-    backgroundColor: "#ffffff",
-  },
-  assetBundlePatterns: ["**/*"],
-  ios: {
-    supportsTablet: true,
-  },
-  android: {
-    adaptiveIcon: {
-      foregroundImage: "./assets/images/adaptive-icon.png",
+  expo: {
+    name: "Ursula",
+    slug: "ursula",
+    version: "1.0.1",
+    orientation: "portrait",
+    icon: "./assets/images/logo-192.png",
+    scheme: "ursula",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/logo.png",
+      resizeMode: "contain",
       backgroundColor: "#ffffff",
     },
-  },
-  web: {
-    bundler: "metro",
-    favicon: "./assets/images/favicon.png",
-  },
-  extra: {
-    supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
-  },
-  expo: {
-    scheme: "ursula",
-    slug: "ursula",
-    name: "Ursula",
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      icon: "./assets/images/logo-192.png",
+      bundleIdentifier: "com.willbeddow.ursula",
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      bundler: "metro",
+      favicon: "./assets/images/favicon.png",
+    },
     experiments: {
       typedRoutes: true,
     },
-    android: {
-      package: "com.willbeddow.ursula",
-    },
-    ios: {
-      bundleIdentifier: "com.willbeddow.ursula",
-    },
     plugins: ["expo-router"],
     extra: {
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
       eas: {
         projectId: "ac90d169-a022-4526-95a5-95e58f0151a4",
       },
