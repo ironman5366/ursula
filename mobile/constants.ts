239693a24e7c230338791120b97c81d56edab757
Will Beddow <will@willbeddow.com>
2024-03-08 14:00:03 -0500

    chore: 🐖 integrate PostHog

--- mobile/constants.ts
+++ mobile/constants.ts
@@ -7,7 +7,12 @@ export const SUPABASE_ANON_KEY = process.env
 export const SUPABASE_PROJECT_URL =
   process.env.EXPO_PUBLIC_SUPABASE_PROJECT_URL;
 
-if (SUPABASE_ANON_KEY === undefined) {
-  console.log("Supabase key is undefined!");
-  console.log("Env vars", process.env);
+if (!SUPABASE_ANON_KEY || !SUPABASE_PROJECT_URL) {
+  throw new Error("Supabase keys not set");
+}
+
+export const ENVIRONMENT = process.env.EXPO_PUBLIC_ENVIRONMENT;
+
+if (!ENVIRONMENT) {
+  throw new Error("Environment not set");
 }
