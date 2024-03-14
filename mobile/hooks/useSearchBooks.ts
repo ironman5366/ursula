a3dd7d2549c35daee7d57a824c08a932974da9d4
Will Beddow <will@willbeddow.com>
2024-03-14 11:31:29 -0400

    WIP on query batching

--- mobile/hooks/useSearchBooks.ts
+++ mobile/hooks/useSearchBooks.ts
@@ -3,17 +3,29 @@ import { Book } from "../../shared-types/derived";
 import { supabase } from "../utils/supabase.ts";
 
 async function fetchSearchBooks({ name }: { name: string }): Promise<Book[]> {
-  console.log("SEarching", name);
-  const { data, error } = await supabase.rpc("search_book_titles", {
+  console.log("Searching", name);
+  const { data, error } = (await supabase.rpc("search_book_titles", {
     search_text: name,
-  });
+  })) as { data: { book: Book; rank: number }[]; error: Error };
 
   if (error) {
     throw error;
   }
   console.log("Got results", data);
 
-  return data.map((d: { book: Book; rank: number }) => d.book);
+  const includesPopularBook = data.some(
+    ({ book }: { book: Book; rank: number }) => book.popularity > 0
+  );
+
+  // Books with popularity = 0 tend to be... messy data. So if we have other
+  // options, we'll filter them out
+  const books = includesPopularBook
+    ? data.filter(
+        ({ book }: { book: Book; rank: number }) => book.popularity > 0
+      )
+    : data;
+
+  return books.map(({ book }: { book: Book; rank: number }) => book);
 }
 
 export default function useSearchBooks({
