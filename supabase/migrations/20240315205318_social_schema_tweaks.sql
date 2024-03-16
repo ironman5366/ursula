ffc69d53cdd803acc9316f5a17996b2732862fc9
Will Beddow <will@willbeddow.com>
2024-03-16 14:19:38 -0400

    social feed wip

--- supabase/migrations/20240315205318_social_schema_tweaks.sql
+++ supabase/migrations/20240315205318_social_schema_tweaks.sql
@@ -1,6 +1,6 @@
 ALTER TABLE activities RENAME COLUMN "activity_type" TO "type";
 ALTER TABLE activities RENAME COLUMN "activity_data" TO "data";
 
-CREATE OR REPLACE FUNCTION social_feed(user_id UUID) RETURNS SETOF ACTIVITIES LANGUAGE plpgsql AS $$
-    SELECT * FROM activities JOIN follows ON activities.user_id = follows.follower_id WHERE follows.followee_id = user_id;
-$$
+CREATE OR REPLACE FUNCTION social_feed(for_user_id UUID) RETURNS SETOF ACTIVITIES LANGUAGE sql AS $$
+    SELECT * FROM activities JOIN follows ON activities.user_id = follows.follower_id WHERE follows.followee_id = for_user_id;
+$$;
