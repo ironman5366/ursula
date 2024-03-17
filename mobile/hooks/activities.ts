1f99f81f849da61dc3744cbdd9a8bb731dd1e4bf
Will Beddow <will@willbeddow.com>
2024-03-17 14:15:45 -0400

    more wip

--- mobile/hooks/activities.ts
+++ mobile/hooks/activities.ts
@@ -1,7 +1,8 @@
 import { Activity } from "@ursula/shared-types/derived.ts";
-import { ActivityData, ActivityType } from "@ursula/shared-types/Activity.ts";
+import { ActivityData } from "@ursula/shared-types/Activity.ts";
 import { supabase } from "../utils/supabase.ts";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useSession } from "../contexts/SessionContext.ts";
 
 async function recordActivity(
   userId: string,
@@ -21,6 +22,17 @@ async function recordActivity(
   }
 
   return activity;
+}
+
+export function useRecordActivity() {
+  const { session } = useSession();
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: ActivityData) => recordActivity(session.user.id, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["ACTIVITIES", session.user.id]);
+    },
+  });
 }
 
 async function fetchUserActivities(userId: string): Promise<Activity[]> {
@@ -43,5 +55,3 @@ export function useActivities(userId: string) {
     queryFn: () => fetchUserActivities(userId),
   });
 }
-
-async function fetchFollowerActivities(userId: string): Promise<Activity[]> {}
--- supabase/migrations/20240316233216_search_updates.sql
+++ supabase/migrations/20240316233216_search_updates.sql
@@ -4,6 +4,10 @@ ON public.books USING gin (to_tsvector('english'::regconfig, title));
 CREATE INDEX IF NOT EXISTS idx_fts_author_name
 ON public.authors USING gin (to_tsvector('english'::regconfig, name));
 
+CREATE INDEX IF NOT EXISTS idx_fts_profiles_name
+ON public.profiles USING gin (to_tsvector('english'::regconfig, full_name));
+
+CREATE INDEX idx_users_username_lower ON users (lower(username));
 
 CREATE OR REPLACE VIEW v_search AS (
     (
@@ -24,5 +28,25 @@ CREATE OR REPLACE VIEW v_search AS (
             name AS search_field,
             1 AS order_key
         FROM authors
+    )
+    UNION
+    (
+        SELECT
+            id AS entity_id,
+            'profiles' AS entity_type,
+            'profiles_name' AS result_type,
+            full_name AS search_field,
+            1 AS order_key
+        FROM profiles
+    )
+    UNION
+    (
+        SELECT
+            id AS entity_id,
+            'profiles_username' AS result_type,
+            username AS search_field,
+            1 AS order_key,
+            con
+            'profiles' AS entity_type
     )
 );
