9c3677f73e9748b7d912c604e0477bc3481444ce
Will Beddow <will@willbeddow.com>
2024-03-22 12:53:11 -0400

    small tweak

--- mobile/app/(onboard)/follows.tsx
+++ mobile/app/(onboard)/follows.tsx
@@ -1,11 +1,14 @@
 import React, { Dispatch, SetStateAction, useState } from "react";
-import { SizableText, Text, YStack } from "tamagui";
+import { Button, SizableText, Text, YStack } from "tamagui";
 import SearchPage from "../../pages/Search";
 import { WILLS_USER_ID } from "../../constants.ts";
 import { useProfile } from "../../hooks/profile.ts";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import LoaderRow from "../../components/atoms/LoaderRow.tsx";
 import ProfilePreviewRow from "../../components/molecules/ProfilePreview/Row.tsx";
+import { FloatingActionBar } from "../../components/containers/TabBar.tsx";
+import { Link } from "expo-router";
+import { MoveRight } from "@tamagui/lucide-icons";
 
 interface FollowProfileItemProps {
   profileId: string;
@@ -61,6 +64,27 @@ export function FindFollowers() {
           );
         }}
       />
+      <FloatingActionBar>
+        <Link href="/" asChild>
+          <Button
+            width={300}
+            unstyled
+            alignSelf="center"
+            height={50}
+            px={10}
+            fontWeight="bold"
+            color="white"
+            href="/(onboard)/signup"
+            flexDirection="row"
+            alignItems="center"
+            alignContent="space-between"
+            justifyContent="space-between"
+            iconAfter={<MoveRight size="$1" />}
+          >
+            Get Started
+          </Button>
+        </Link>
+      </FloatingActionBar>
     </YStack>
   );
 }
