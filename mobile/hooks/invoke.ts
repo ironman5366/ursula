d4ceea8a701bde3b90f5171c183aa6422fa45d2f
Will Beddow <will@willbeddow.com>
2024-03-23 14:00:48 -0400

    done! :)

--- mobile/hooks/invoke.ts
+++ mobile/hooks/invoke.ts
@@ -4,45 +4,45 @@ import {
   LLMMessageDelta,
   LLMResponseStream,
 } from "@ursula/shared-types/llm.ts";
-import { SUPABASE_PROJECT_URL } from "../constants.ts";
-import { fetchEventSource } from "@microsoft/fetch-event-source";
-
-async function queueDeltas(
-  params: InvocationParams,
-  queue: LLMMessageDelta[],
-  close: (reason: LLMFinishReason) => void
-) {
-  const functionUrl = `${SUPABASE_PROJECT_URL}/functions/v1/invoke-ai/`;
-
-  await fetchEventSource(functionUrl, {
-    method: "POST",
-    body: JSON.stringify(params),
-    onmessage: (event) => {
-      const delta = JSON.parse(event.data);
-      queue.push(delta);
-    },
-    onclose: () => close(LLMFinishReason.FINISHED),
-  });
-}
+import { SUPABASE_ANON_KEY, SUPABASE_PROJECT_URL } from "../constants.ts";
+import EventSource from "react-native-sse";
 
 export async function* invoke(params: InvocationParams): LLMResponseStream {
-  // This is a supabase function but because it uses SSE we'll invoke it on our own
+  const functionUrl = `${SUPABASE_PROJECT_URL}/functions/v1/invoke-ai/`;
+  const eventSource = new EventSource(functionUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
+    },
+    body: JSON.stringify(params),
+  });
 
-  let queue: LLMMessageDelta[] = [];
-  let closeReason: LLMFinishReason | null = null;
+  let messageQueue: LLMMessageDelta[] = [];
+  let finishReason: LLMFinishReason | null = null;
 
-  queueDeltas(params, queue, (reason) => (closeReason = reason));
+  eventSource.addEventListener("error", (event) => {
+    console.error(event);
+    throw event;
+  });
+
+  eventSource.addEventListener("message", (message) => {
+    messageQueue.push(JSON.parse(message.data));
+  });
+
+  eventSource.addEventListener("close", (event) => {
+    // TODO: propagate this
+    finishReason = LLMFinishReason.FINISHED;
+  });
 
   while (true) {
-    if (closeReason) {
-      return closeReason;
+    if (messageQueue.length > 0) {
+      yield messageQueue.shift() as LLMMessageDelta;
+    } else {
+      await new Promise((resolve) => setTimeout(resolve, 50));
     }
 
-    if (queue.length > 0) {
-      const delta = queue.shift();
-      yield delta;
-    } else {
-      await new Promise((resolve) => setTimeout(resolve, 100));
+    if (finishReason) {
+      return finishReason;
     }
   }
 }
