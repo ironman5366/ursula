324053010a987874d4a972ea4594882a99a2abd8
Will Beddow <will@willbeddow.com>
2024-03-26 11:44:27 -0400

    more integration wip

--- mobile/pages/Chat/index.tsx
+++ mobile/pages/Chat/index.tsx
@@ -1,22 +1,47 @@
 import { Send } from "@tamagui/lucide-icons";
 import LLM from "@ursula/shared-types/llm.ts";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { KeyboardAvoidingView, SafeAreaView } from "react-native";
 import { Button, XStack, YStack } from "tamagui";
 import { useInvoke } from "../../ai/invoke.ts";
 import StyledInput from "../../components/atoms/StyledInput.tsx";
-import ChatMessage, { AssistantMessage } from "./ChatMessage";
+import ChatMessage from "./ChatMessage";
 import Model = LLM.Model;
+import ReviewWithBook from "../../types/ReviewWithBook.ts";
+import { useReviews } from "../../hooks/reviews.ts";
+import { useSession } from "../../contexts/SessionContext.ts";
+import LoadingScreen from "../../components/atoms/LoadingScreen.tsx";
 
 export default function ChatPage() {
   const [input, setInput] = useState("");
+  const { session } = useSession();
+  const { data: reviews, isLoading } = useReviews(session.user.id);
+
+  const systemMessage = useMemo(() => {
+    let systemReviews = reviews || [];
+    const initialPrompt =
+      "You're a librarian, helping a user choose a book to read.";
+
+    if (systemReviews.length === 0) {
+      return initialPrompt;
+    } else {
+      const books = reviews.map(({ book, review }, i) => {
+        return `#${i + 1}: ${book.title}`;
+      });
+      return `${initialPrompt}\nHere are some books they enjoy, in order:\n${books}`;
+    }
+  }, [reviews]);
+
+  console.log(systemMessage);
+
   const { messages, isInvoking, addMessage } = useInvoke({
     model: Model.ANTHROPIC_HAIKU,
+    systemMessage,
   });
 
-  useEffect(() => {
-    console.log(messages);
-  }, [messages]);
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
 
   return (
     <KeyboardAvoidingView
@@ -55,7 +80,7 @@ export default function ChatPage() {
               p="$1"
               backgroundColor="blue"
               icon={<Send size={20} color="white" />}
-            ></Button>
+            />
           </XStack>
         </YStack>
       </SafeAreaView>
