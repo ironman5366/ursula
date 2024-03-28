42d20bf7589470b02ffc07470415cebd26ff9a9f
Will Beddow <will@willbeddow.com>
2024-03-27 20:46:57 -0400

    Another delete_bs_authors migration

---
+++ supabase/migrations/20240328004430_more_bs_deletion.sql
@@ -0,0 +1,5 @@
+SELECT delete_bs_author('OL9780495A'); -- Instaread summaries staff
+SELECT delete_bs_author('OL9312258A'); -- Bright bright summaries
+SELECT delete_bs_author('OL9102926A'); -- Worth books summaries
+SELECT delete_bs_author('OL7547594A'); -- Cengage Learning Gale
+SELECT delete_bs_author('OL8354734A'); -- SuperSummary
