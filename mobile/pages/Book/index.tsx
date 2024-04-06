2ab8c78d8f0bcc0e1384648b608815c0b4f565aa
Will Beddow <will@willbeddow.com>
2024-04-06 01:14:57 -0400

    add activity feed to book detail

--- mobile/pages/Book/index.tsx
+++ mobile/pages/Book/index.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Book } from "@ursula/shared-types/derived.ts";
 import { Stack } from "expo-router";
-import { XStack, YStack } from "tamagui";
+import { Text, XStack, YStack } from "tamagui";
 import ReadingListButton from "../../app/(app)/bookDetail/ReadingListButton.tsx";
 import ReviewButton from "../../app/(app)/bookDetail/ReviewButton.tsx";
 import { StyledText } from "../../components/atoms/StyledText.tsx";
 import BookHeader from "./Header.tsx";
-import { ScrollView } from "react-native";
+import { ActivityIndicator, ScrollView } from "react-native";
 import MarkReadingButton from "../../app/(app)/bookDetail/MarkReadingButton.tsx";
 import { useUserBookReview } from "../../hooks/reviews.ts";
+import { useBookSocialFeed } from "../../hooks/activities.ts";
+import ActivityFeed from "../../components/organisms/ActivityFeed";
 
 interface Props {
   book: Book;
 }
 
 export default function BookDetailPage({ book }: Props) {
-  const { data: review, isLoading: isReviewLoading } = useUserBookReview(
-    book.id
-  );
+  const { data: review } = useUserBookReview(book.id);
+  const { data: activities, isLoading } = useBookSocialFeed(book.id);
 
   return (
     <>
@@ -42,8 +43,17 @@ export default function BookDetailPage({ book }: Props) {
           {!review && <MarkReadingButton book={book} />}
         </XStack>
         <ScrollView>
-          <YStack px="$3" py="$3">
+          <YStack px="$3" py="$3" gap={"$5"} alignItems={"center"}>
             <StyledText>{book.description}</StyledText>
+            {isLoading && <ActivityIndicator size={"small"} />}
+            {Array.isArray(activities) && activities.length > 1 && (
+              <>
+                <Text fontSize={"$5"} fontWeight={"bold"}>
+                  What Your Friends Are Saying
+                </Text>
+                <ActivityFeed activities={activities} />
+              </>
+            )}
           </YStack>
         </ScrollView>
       </YStack>
