bcb502057c3450c557e16994f38323d9b1fe20cf
Will Beddow <will@willbeddow.com>
2024-04-05 23:12:16 -0400

    small functions fix

--- supabase/functions/invoke-ai/providers/anthropic/utils.ts
+++ supabase/functions/invoke-ai/providers/anthropic/utils.ts
@@ -1,22 +1,5 @@
 import { create } from "npm:xmlbuilder2@^3.1.1";
-import { FunctionCalls } from "./types.ts";
 
 export function jsonToXml(jsonObj: any, rootName: string = "root"): string {
-  const xml = create({ version: "1.0" })
-    .ele(rootName)
-    .ele(jsonObj)
-    .end({ prettyPrint: true });
-
-  return xml;
+  return create().ele(rootName).ele(jsonObj).end({ prettyPrint: true });
 }
-
-// export function genFunctionXml(tool_name: string, parameters:  Parameter | Parameter[]) {
-//   const functionCalls: FunctionCalls = {
-//     invoke: {
-//       tool_name,
-//       parameters: parameters,
-//     },
-//   };
-
-//   return jsonToXml(functionCalls, 'function_calls');
-// }
